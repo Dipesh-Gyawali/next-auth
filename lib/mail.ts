43f6b323682c36f919128fe2935595be95c1f912
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-12-04 10:08:37 +0545

    url fixed

--- lib/mail.ts
+++ lib/mail.ts
@@ -14,7 +14,7 @@ export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
 
 //this is to reset the password if forgotten
 export const sendPasswordResetEmail = async (email: string, token: string) => {
-  const resetLink = `http://localhost:3000/auth/new-password?token=${token}`;
+  const resetLink = `https://next-auth-mw31.vercel.app/auth/new-password?token=${token}`;
 
   await resend.emails.send({
     from: "onboarding@resend.dev",
@@ -26,7 +26,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
 
 //this is to verify the email while register
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+  const confirmLink = `https://next-auth-mw31.vercel.app/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
     from: "onboarding@resend.dev",
