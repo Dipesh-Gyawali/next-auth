52a33f989fe12a4ae3714e352ce800b9021158db
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-12-04 12:28:08 +0545

    random2

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -3,7 +3,7 @@ import React, { Suspense } from "react";
 
 const LoginPage = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<div>Loading..</div>}>
       <LoginForm />
     </Suspense>
   );
